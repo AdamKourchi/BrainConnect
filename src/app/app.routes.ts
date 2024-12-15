866da358e92bf3a94e9cf2782a5ded4987d9f70c
Youssef <mryoussefoubihi@gmail.com>
2024-12-15 20:32:09 +0100

    update nav-bar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,13 @@
-import {Routes} from '@angular/router';
-import {HomeComponent} from './home/home.component';
-import {LoginComponent} from './login/login.component';
-import {SignupComponent} from './signup/signup.component';
-import {CreateComponent} from './create/create.component';
+import { Routes } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { LoginComponent } from "./login/login.component";
+import { SignupComponent } from "./signup/signup.component";
+import { CreateComponent } from "./create/create.component";
 
 export const routes: Routes = [
-  {path: "", component: HomeComponent},
-  {path: "login", component: LoginComponent},
-  {path: "signup", component: SignupComponent},
-  {path: "create", component: CreateComponent},
-  {path: '**', redirectTo: '/'},
+  { path: "", component: HomeComponent },
+  { path: "login", component: LoginComponent },
+  { path: "signup", component: SignupComponent },
+  { path: "create", component: CreateComponent },
+  { path: "**", redirectTo: "/" },
 ];
