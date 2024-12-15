089719c0e2aa30e7101d0c82f640816cf16ec0e0
Youssef <mryoussefoubihi@gmail.com>
2024-12-16 00:23:20 +0100

    Implement Profile page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from "./home/home.component";
 import { LoginComponent } from "./login/login.component";
 import { SignupComponent } from "./signup/signup.component";
 import { CreateComponent } from "./create/create.component";
+import { ProfileComponent } from "./profile/profile.component";
 
 export const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "login", component: LoginComponent },
   { path: "signup", component: SignupComponent },
   { path: "create", component: CreateComponent },
+  { path: "profile", component: ProfileComponent },
   { path: "**", redirectTo: "/" },
 ];
---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,34 @@
+import { Component } from "@angular/core";
+import { canvasData } from "../Data";
+import { MatIconModule } from "@angular/material/icon";
+import { ReactiveFormsModule, FormGroup, FormControl } from "@angular/forms";
+
+@Component({
+  selector: "app-profile",
+  imports: [MatIconModule, ReactiveFormsModule],
+  templateUrl: "./profile.component.html",
+  styleUrl: "./profile.component.css",
+})
+export class ProfileComponent {
+  canvas = canvasData;
+
+  formGroup = new FormGroup({
+    email: new FormControl("ayoub123@gmail.com"),
+    userName: new FormControl("Ayoub"),
+    password: new FormControl("this is my password"),
+    rePassword: new FormControl("this is my password again"),
+  });
+
+  showMessage = false;
+
+  onSubmit() {
+    if (this.formGroup.status === "VALID") {
+      this.showMessage = true;
+      setTimeout(() => (this.showMessage = false), 3000);
+    }
+  }
+
+  onDelete(id: string) {
+    this.canvas = this.canvas.filter((ca) => ca.id !== id);
+  }
+}
